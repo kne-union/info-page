e3822192acf18598d9978e408b7db4caf0110b51
zhipenglin <zhipeng.lin@leapin.io>
2025-04-27 10:32:21 +0800

    添加report

---
+++ src/Report/PrintPageBreak.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './style.module.scss';
+
+const PrintPageBreak = () => {
+  return <div className={style['print-page-break']} />;
+};
+
+export default PrintPageBreak;
--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as CentralContent } from './CentralContent';
 export { default as TableView } from './TableView';
 export { default as formatView, defaultFormat } from './formatView';
 export { default as Flow } from './Flow';
+export { default as Report } from './Report';
